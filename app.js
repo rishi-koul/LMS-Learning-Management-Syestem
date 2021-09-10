135d5442a89c7a1dadb1fca722b115a5a558500a
rishi-koul <rishikoul2@gmail.com>
2021-09-11 01:01:08 +0530

    test

--- app.js
+++ app.js
@@ -225,4 +225,6 @@ app.post("/profile", async function(req, res){
     })
 })
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
